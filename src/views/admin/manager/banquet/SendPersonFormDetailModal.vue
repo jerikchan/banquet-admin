d87f9f544286b525e1c7db144d140f3ccb9355c5
jerikchan <jerikchan@gmail.com>
2021-09-02 10:38:05 +0800

    fix: build err [release]

--- src/views/admin/manager/banquet/SendPersonFormDetailModal.vue
+++ src/views/admin/manager/banquet/SendPersonFormDetailModal.vue
@@ -2,7 +2,7 @@
   <PageWrapper title="人员派单登记表详情" contentBackground @back="goBack">
     <template #extra>
       <div class="flex">
-        <a-button class="mr-2"type="primary" @click="exportForm"> 导出表格 </a-button>
+        <a-button class="mr-2" type="primary" @click="exportForm"> 导出表格 </a-button>
         <BasicUpload
           :maxSize="20"
           :maxNumber="10"
